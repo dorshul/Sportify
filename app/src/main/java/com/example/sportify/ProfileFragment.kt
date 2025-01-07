a81f84f023eb4390d19f8eebffe07dfd72bec8b0
dorshul <dshulshtain@gmail.com>
2025-01-08 00:08:29 +0200

    Add ProfileFragment

---
+++ app/src/main/java/com/example/sportify/ProfileFragment.kt
@@ -0,0 +1,71 @@
+package com.example.sportify
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.sportify.databinding.FragmentProfileBinding
+import com.example.sportify.databinding.FragmentStudentsListBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ProfileFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ProfileFragment : Fragment() {
+    private var binding: FragmentProfileBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        binding?.nameEditButton?.setOnClickListener {
+            if (binding?.nameEditText?.visibility == View.GONE) {
+                // Switch to EditText
+                binding?.nameEditText?.setText(binding?.nameTextValue?.text)
+                binding?.nameEditText?.visibility = View.VISIBLE
+                binding?.nameTextValue?.visibility = View.GONE
+                binding?.nameEditButton?.setImageResource(R.drawable.ic_save) // Change to save icon
+            } else {
+                // Save changes and switch back to TextView
+                binding?.nameTextValue?.text = binding?.nameEditText?.text.toString()
+                binding?.nameEditText?.visibility = View.GONE
+                binding?.nameTextValue?.visibility = View.VISIBLE
+                binding?.nameEditButton?.setImageResource(R.drawable.ic_edit) // Change back to edit icon
+            }
+        }
+
+        binding?.ageEditButton?.setOnClickListener {
+            if (binding?.ageEditText?.visibility == View.GONE) {
+                // Switch to EditText
+                binding?.ageEditText?.setText(binding?.ageTextValue?.text)
+                binding?.ageEditText?.visibility = View.VISIBLE
+                binding?.ageTextValue?.visibility = View.GONE
+                binding?.ageEditButton?.setImageResource(R.drawable.ic_save) // Change to save icon
+            } else {
+                // Save changes and switch back to TextView
+                binding?.ageTextValue?.text = binding?.ageEditText?.text.toString()
+                binding?.ageEditText?.visibility = View.GONE
+                binding?.ageTextValue?.visibility = View.VISIBLE
+                binding?.ageEditButton?.setImageResource(R.drawable.ic_edit) // Change back to edit icon
+            }
+        }
+        
+        return binding?.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+
+}
