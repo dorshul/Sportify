3e73b72077cb7576d8830a62ed4ce7e5d85e1371
dorshul <dshulshtain@gmail.com>
2025-01-15 17:29:22 +0200

    Add camera launcher

--- app/src/main/java/com/example/sportify/AddGameFragment.kt
+++ app/src/main/java/com/example/sportify/AddGameFragment.kt
@@ -1,5 +1,7 @@
 package com.example.sportify
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.text.Editable
 import android.util.Log
@@ -7,6 +9,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.navigation.Navigation
 import com.example.sportify.databinding.FragmentAddGameBinding
 import com.example.sportify.model.Model
@@ -14,8 +19,10 @@ import com.example.sportify.model.Game
 import java.util.UUID
 
 class AddGameFragment : Fragment() {
+    private var cameraLauncher: ActivityResultLauncher<Void?>? = null
     private var binding: FragmentAddGameBinding? = null
     var game: Game? = null
+    var previousBitmap: Bitmap? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,7 +30,24 @@ class AddGameFragment : Fragment() {
     ): View? {
         binding = FragmentAddGameBinding.inflate(inflater, container, false)
         binding?.saveButton?.setOnClickListener(::onSaveClicked)
-        val gameId = arguments?.let { AddGameFragmentArgs.fromBundle(it).gameId }
+        val drawable = ContextCompat.getDrawable(requireContext(), R.drawable.take_picture)
+        previousBitmap = (drawable as BitmapDrawable).bitmap
+        cameraLauncher = registerForActivityResult((ActivityResultContracts.TakePicturePreview())) { bitmap ->
+            if (bitmap != null) {
+                // Update the ImageView with the new image and save it as the previous image
+                previousBitmap = bitmap
+                binding?.takePictureImageView?.setImageBitmap(bitmap)
+            } else {
+                // Camera was closed, restore the previous image
+                binding?.takePictureImageView?.setImageBitmap(previousBitmap)
+            }
+        }
+        binding?.takePictureImageView?.setOnClickListener {
+            cameraLauncher?.launch(null)
+        }
+        val args = AddGameFragmentArgs.fromBundle(requireArguments())
+        val gameId = args.gameId
+//        val gameId = arguments?.let { AddGameFragmentArgs.fromBundle(requireArguments()).gameId }
         if (gameId != null) {
             Model.shared.getGameById(gameId ?: "") {
                 game = it
@@ -41,7 +65,7 @@ class AddGameFragment : Fragment() {
         game = Game(
             id = game?.id ?: UUID.randomUUID().toString(),
             userId =  game?.userId ?: UUID.randomUUID().toString(),
-            pictureUrl = game?.pictureUrl ?:"@drawable/take_picture",
+            pictureUrl = game?.pictureUrl ?: "@drawable/take_picture",
             approvals = game?.approvals ?: 0,
             location = binding?.locationText?.text?.toString() ?: "",
             description = binding?.descriptionText?.text?.toString() ?: "",
@@ -53,13 +77,4 @@ class AddGameFragment : Fragment() {
             Navigation.findNavController(view).popBackStack()
         }
     }
-    override fun onResume() {
-        super.onResume()
-        getGame()
-    }
-
-    private fun getGame() {
-
-    }
-
 }
--- app/src/main/java/com/example/sportify/ProfileFragment.kt
+++ app/src/main/java/com/example/sportify/ProfileFragment.kt
@@ -1,21 +1,42 @@
 package com.example.sportify
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.example.sportify.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
     private var binding: FragmentProfileBinding? = null
+    private var cameraLauncher: ActivityResultLauncher<Void?>? = null
+    var previousBitmap: Bitmap? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentProfileBinding.inflate(inflater, container, false)
-
+        val drawable = ContextCompat.getDrawable(requireContext(), R.drawable.avatar)
+        previousBitmap = (drawable as BitmapDrawable).bitmap
+        cameraLauncher = registerForActivityResult((ActivityResultContracts.TakePicturePreview())) { bitmap ->
+            if (bitmap != null) {
+                // Update the ImageView with the new image and save it as the previous image
+                previousBitmap = bitmap
+                binding?.userPicture?.setImageBitmap(bitmap)
+            } else {
+                // Camera was closed, restore the previous image
+                binding?.userPicture?.setImageBitmap(previousBitmap)
+            }
+        }
+        binding?.editUserPictureButton?.setOnClickListener {
+            cameraLauncher?.launch(null)
+        }
         binding?.nameEditButton?.setOnClickListener {
             if (binding?.nameEditText?.visibility == View.GONE) {
                 // Switch to EditText
